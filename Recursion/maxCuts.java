fc418c46ac39c0fee9c937eb7b787f232267e9e0
Buhari Nasir Ahmad <buharinasir1996@gmail.com>
2024-03-20 02:27:23 +0530

    Create maxCuts.java

---
+++ Recursion/maxCuts.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		System.out.println(maxCuts(5,2,1,5));
+	}
+	
+	static int maxCuts(int n, int a, int b, int c){
+		    if(n == 0) return 0;
+		    if(n <= -1) return -1;
+		    
+		    int res =  Math.max(maxCuts(n - a, a, b, c),
+		                    Math.max(maxCuts(n - b, a, b, c),
+		                             maxCuts(n -c, a, b, c)));
+		                             
+		  if(res == -1) return -1;
+		  return res + 1;
+		}
+}
